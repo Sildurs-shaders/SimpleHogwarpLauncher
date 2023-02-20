42ef88bfa4261ac711d50f41905b2894438441cc
Sildur <edhuin@gmx.ch>
2023-02-20 04:24:59 +0100

    Add files via upload

---
+++ SimpleHogwarpLauncher/SimpleHogwarpLauncher.bat
@@ -0,0 +1,19 @@
+@echo off
+	Title SimpleHogwarpLauncher
+	rem made by Sildur: https://twitter.com/Sildurs_shaders https://github.com/Sildurs-shaders
+	Set "LauncherPath=%~dp0\"
+@echo Injecting Hogwarp and launching the game..
+	move "%LauncherPath%Polyjuice" "%LauncherPath%..\Polyjuice"
+	move "%LauncherPath%Phoenix\Binaries\Win64\version.dll" "%LauncherPath%..\Phoenix\Binaries\Win64\"
+ 	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp.pak" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp.ucas" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp.utoc" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+	timeout /T 1 /NOBREAK > nul
+@echo Waiting for hogwarts legacy to end before moving the files back..	
+	start /W /D "%LauncherPath%..\.." HogwartsLegacy.exe -SaveToUserDir -UserDir="Hogwarts Legacy" /seperate
+	move "%LauncherPath%..\Polyjuice" "%LauncherPath%Polyjuice"
+	move "%LauncherPath%..\Phoenix\Binaries\Win64\version.dll" "%LauncherPath%Phoenix\Binaries\Win64\"
+ 	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp.pak" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp.ucas" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp.utoc" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+@echo bye
