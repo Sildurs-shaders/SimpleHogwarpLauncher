67e41ae21e9dc98739dba4cd3259facf583dea8d
Sildur <edhuin@gmx.ch>
2023-02-27 00:14:29 +0100

    Update SimpleHogwarpLauncher.bat
    Updated to support hogwarp v0.21

--- SimpleHogwarpLauncher/SimpleHogwarpLauncher.bat
+++ SimpleHogwarpLauncher/SimpleHogwarpLauncher.bat
@@ -5,15 +5,17 @@
 @echo Injecting Hogwarp and launching the game..
 	move "%LauncherPath%Polyjuice" "%LauncherPath%..\Polyjuice"
 	move "%LauncherPath%Phoenix\Binaries\Win64\version.dll" "%LauncherPath%..\Phoenix\Binaries\Win64\"
- 	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp.pak" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
-	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp.ucas" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
-	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp.utoc" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+ 	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp_P.pak" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp_P.ucas" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarp_P.utoc" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%Phoenix\Content\Paks\~mods\HogWarpOverride_P.pak" "%LauncherPath%..\Phoenix\Content\Paks\~mods\"
 	timeout /T 1 /NOBREAK > nul
 @echo Waiting for hogwarts legacy to end before moving the files back..	
 	start /W /D "%LauncherPath%..\.." HogwartsLegacy.exe -SaveToUserDir -UserDir="Hogwarts Legacy" /seperate
 	move "%LauncherPath%..\Polyjuice" "%LauncherPath%Polyjuice"
 	move "%LauncherPath%..\Phoenix\Binaries\Win64\version.dll" "%LauncherPath%Phoenix\Binaries\Win64\"
- 	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp.pak" "%LauncherPath%Phoenix\Content\Paks\~mods\"
-	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp.ucas" "%LauncherPath%Phoenix\Content\Paks\~mods\"
-	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp.utoc" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+ 	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp_P.pak" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp_P.ucas" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarp_P.utoc" "%LauncherPath%Phoenix\Content\Paks\~mods\"
+	move "%LauncherPath%..\Phoenix\Content\Paks\~mods\HogWarpOverride_P.pak" "%LauncherPath%Phoenix\Content\Paks\~mods\"
 @echo bye
